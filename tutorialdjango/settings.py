b78204b3d668efcbf13b0f6521788b7508c9dffa
AlimNu <noeralim21@gmail.com>
2021-07-22 14:54:26 +0800

    connecting from django to flask service on docker success

--- tutorialdjango/settings.py
+++ tutorialdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$c&kje*bxkkhga4t3a1=^&6$g!#i(xm=rjw#i_52^app@-5uci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
